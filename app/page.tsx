b0e142c82712ea9363560ea6246d7abd6e8223f3
Emanoel Mendes <emanoelims@hotmail.com>
2024-09-04 08:35:29 -0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Camera } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Button>Hello</Button>
+      <Camera color="blue" size={48} />
+    </>
+  );
+}
